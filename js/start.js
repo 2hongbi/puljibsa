bb2a8245a9f9ae6d66ca08574679511464b3cdf4
Songhee120 <clasonghee@gamil.com>
2022-02-20 12:08:48 +0900

    add jquery in start.js

--- js/start.js
+++ js/start.js
@@ -1,3 +1,8 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 const main = document.querySelector("#main")
 const qna1 = document.querySelector("#qna1")
 const qna2 = document.querySelector("#qna2")
