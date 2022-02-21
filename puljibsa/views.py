78ff5deb33c10cd46c28ccfb6fbef30188105151
hongbi-lee <foxgirin950@gmail.com>
2022-02-22 00:29:36 +0900

    edit views.py and index.html

--- puljibsa/views.py
+++ puljibsa/views.py
@@ -4,7 +4,18 @@ from .models import Question, Answer
 
 
 def index(request):
-    return render(request, 'puljibsa/index.html')
+    qs_1 = Question.objects.get(id=1)
+    qs_2 = Question.objects.get(id=2)
+    qs_3 = Question.objects.get(id=3)
+    qs_4 = Question.objects.get(id=4)
+
+    context = {
+        'qs_1': qs_1,
+        'qs_2': qs_2,
+        'qs_3': qs_3,
+        'qs_4': qs_4,
+    }
+    return render(request, 'puljibsa/index.html', context)
 
 
 def intro(request):
