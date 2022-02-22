cbda73eb5382a3aa036ec74f22a93e6f50ddef3a
hongbi-lee <foxgirin950@gmail.com>
2022-02-22 21:50:37 +0900

    add Plant & PlantStatus Models and add data to them

--- puljibsa/admin.py
+++ puljibsa/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Question, Answer
+from .models import Question, Answer, Plant, PlantStatus
 
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Plant)
+admin.site.register(PlantStatus)
--- puljibsa/models.py
+++ puljibsa/models.py
@@ -18,3 +18,26 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.answer_text
+
+
+class Plant(models.Model):
+    plt_cd = models.CharField(unique=True, max_length=10)
+    plt_nm = models.CharField(max_length=100)
+    plt_enm = models.CharField(max_length=100)
+    plt_exp = models.TextField()    # 한줄 설명
+    plt_water = models.TextField()
+    plt_amount_water = models.TextField()
+    plt_location = models.TextField()
+    plt_disease = models.TextField()
+
+    def __str__(self):
+        return self.plt_nm
+
+
+class PlantStatus(models.Model):
+    plants = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    plt_status_content = models.TextField()
+
+    def __str__(self):
+        return self.plants.plt_nm + '/' + self.answer.answer_text + ' : ' + self.plt_status_content
